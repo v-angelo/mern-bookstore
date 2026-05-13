6ea358daf1bf4cb82db06f192de06660eeab6f45
v-angelo <v.angelo003@gmail.com>
2026-05-13 10:17:46 +0530

    server url changed

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://mern-bookstore-server-j25y.onrender.com",
   timeout: 10000,
 });
 
